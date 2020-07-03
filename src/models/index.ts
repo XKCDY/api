a4d6184749b12334ccb11fcd6b185cdd7930aea5
Max Isom <hi@maxisom.me>
2020-07-02 20:26:31 -0500

    Use DATABASE_URL

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,22 +2,7 @@ import * as sequelize from 'sequelize';
 import {ComicFactory} from './comic';
 import {ComicImgFactory} from './comic-img';
 
-export const db = new sequelize.Sequelize(
-  'postgres',
-  'postgres',
-  '',
-  {
-    port: Number(process.env.DB_PORT) ?? 5432,
-    host: process.env.DB_HOST ?? 'localhost',
-    dialect: 'postgres',
-    pool: {
-      min: 0,
-      max: 5,
-      acquire: 30000,
-      idle: 10000
-    }
-  }
-);
+export const db = new sequelize.Sequelize(process.env.DATABASE_URL ?? 'postgres://postgres:postgres@localhost/postgres');
 
 export const Comic = ComicFactory(db);
 export const ComicImg = ComicImgFactory(db);
