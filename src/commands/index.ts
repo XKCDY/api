ab4b4722dc28c5b637ade09258e683de8bda2661
Max Isom <hi@maxisom.me>
2020-07-21 21:17:20 -0500

    Order

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,7 +1,7 @@
-import start from './start';
-import scrape from './scrape';
 import migrate from './migrate';
 import migrateAndStart from './migrateAndStart';
+import scrape from './scrape';
 import sendNotifications from './sendNotifications';
+import start from './start';
 
 export {start, scrape, migrate, sendNotifications, migrateAndStart};
