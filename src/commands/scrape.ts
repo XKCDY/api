0be4882773c3128cb45d5707d237c1dd34f10c0b
Max Isom <hi@maxisom.me>
2020-09-09 20:12:45 -0400

    Attempt to scrape multiple comics at once

--- src/commands/scrape.ts
+++ src/commands/scrape.ts
@@ -4,6 +4,8 @@ import * as exifr from 'exifr';
 import {Comic} from '../models';
 import {ComicImgModel, ImgSize} from '../models/comic-img';
 
+const PARALLEL_SCRAPES = 5;
+
 interface XKCDResponse {
   num: number;
   link: string;
@@ -35,81 +37,105 @@ export default async () => {
   }
 
   // Require 2 not found errors in a row to terminate
-  let numOfNotFoundErrors = 0;
+  let idsNotFound: number[] = [];
 
-  while (numOfNotFoundErrors < 2) {
-    try {
-      // eslint-disable-next-line no-await-in-loop
-      const comic: XKCDResponse = await got(`https://xkcd.com/${fetchingId}/info.0.json`).json();
+  const hasScrapedAll = () => {
+    let twoConsecutiveNotFound = false;
 
-      // Get images
-      const promises: Array<Promise<[PromiseSettledResult<ProbeResult>, PromiseSettledResult<ProbeResult>] | number>> = [
-        Promise.allSettled([
-          probe(comic.img),
-          probe(getX2Url(comic.img))
-        ])
-      ];
+    idsNotFound.forEach((id, i) => {
+      const nextId = idsNotFound[i + 1];
 
-      if (comic.img.includes('jpeg') || comic.img.includes('jpg')) {
-        promises.push((async () => {
-          const imgData = await got(comic.img).buffer();
-          return (await exifr.orientation(imgData)) ?? 1;
-        })());
+      if (nextId && nextId === id + 1) {
+        twoConsecutiveNotFound = true;
       }
+    });
 
-      // eslint-disable-next-line no-await-in-loop
-      const resolved = await Promise.all(promises);
-      const imgResults = resolved[0] as Array<PromiseSettledResult<ProbeResult>>;
-      const orientation = resolved[1] as number;
+    return twoConsecutiveNotFound;
+  };
 
-      const swapWidthAndHeight = orientation !== undefined && [5, 6, 7, 8].includes(orientation);
+  while (!hasScrapedAll()) {
+    const idsToScrape = [];
 
-      const imgs: ComicImgModel[] = [];
-
-      imgResults.forEach((result, i) => {
-        if (result.status === 'fulfilled') {
-          const height = swapWidthAndHeight ? result.value.width : result.value.height;
-          const width = swapWidthAndHeight ? result.value.height : result.value.width;
-
-          imgs.push({
-            height,
-            width,
-            ratio: width / height,
-            sourceUrl: result.value.url,
-            size: i === 0 ? ImgSize.x1 : ImgSize.x2
-          } as ComicImgModel);
-        } else if (result.reason.statusCode !== 404) {
-          throw new Error(`Non-404 HTTP code: ${result.reason.statusCode as string}`);
-        }
-      });
-
-      // eslint-disable-next-line no-await-in-loop
-      await Comic.create({
-        id: comic.num,
-        publishedAt: new Date(`${comic.month}-${comic.day}-${comic.year}`),
-        news: comic.news,
-        safeTitle: comic.safe_title,
-        title: comic.title,
-        transcript: comic.transcript,
-        alt: comic.alt,
-        sourceUrl: `https://xkcd.com/${comic.num}`,
-        explainUrl: `https://www.explainxkcd.com/wiki/index.php/${comic.num}`,
-        imgs: imgs
-      }, {
-        include: [Comic.Imgs]
-      });
-    } catch (error) {
-      if (error.constructor === HTTPError) {
-        if (error.response.statusCode === 404) {
-          numOfNotFoundErrors++;
-        } else {
-          throw error;
-        }
-      }
+    for (let i = fetchingId; i < fetchingId + PARALLEL_SCRAPES; i++) {
+      idsToScrape.push(i);
     }
 
-    fetchingId++;
+    // eslint-disable-next-line no-await-in-loop
+    await Promise.all(idsToScrape.map(async id => {
+      try {
+        const comic: XKCDResponse = await got(`https://xkcd.com/${id}/info.0.json`).json();
+
+        // Get images
+        const promises: Array<Promise<[PromiseSettledResult<ProbeResult>, PromiseSettledResult<ProbeResult>] | number>> = [
+          Promise.allSettled([
+            probe(comic.img),
+            probe(getX2Url(comic.img))
+          ])
+        ];
+
+        if (comic.img.includes('jpeg') || comic.img.includes('jpg')) {
+          promises.push((async () => {
+            const imgData = await got(comic.img).buffer();
+            return (await exifr.orientation(imgData)) ?? 1;
+          })());
+        }
+
+        const resolved = await Promise.all(promises);
+        const imgResults = resolved[0] as Array<PromiseSettledResult<ProbeResult>>;
+        const orientation = resolved[1] as number;
+
+        const swapWidthAndHeight = orientation !== undefined && [5, 6, 7, 8].includes(orientation);
+
+        const imgs: ComicImgModel[] = [];
+
+        imgResults.forEach((result, i) => {
+          if (result.status === 'fulfilled') {
+            const height = swapWidthAndHeight ? result.value.width : result.value.height;
+            const width = swapWidthAndHeight ? result.value.height : result.value.width;
+
+            imgs.push({
+              height,
+              width,
+              ratio: width / height,
+              sourceUrl: result.value.url,
+              size: i === 0 ? ImgSize.x1 : ImgSize.x2
+            } as ComicImgModel);
+          } else if (result.reason.statusCode !== 404) {
+            throw new Error(`Non-404 HTTP code: ${result.reason.statusCode as string}`);
+          }
+        });
+
+        await Comic.create({
+          id: comic.num,
+          publishedAt: new Date(`${comic.month}-${comic.day}-${comic.year}`),
+          news: comic.news,
+          safeTitle: comic.safe_title,
+          title: comic.title,
+          transcript: comic.transcript,
+          alt: comic.alt,
+          sourceUrl: `https://xkcd.com/${comic.num}`,
+          explainUrl: `https://www.explainxkcd.com/wiki/index.php/${comic.num}`,
+          imgs: imgs
+        }, {
+          include: [Comic.Imgs]
+        });
+      } catch (error) {
+        if (error.constructor === HTTPError) {
+          if (error.response.statusCode === 404) {
+            idsNotFound.push(id);
+          } else {
+            throw error;
+          }
+        }
+      }
+    }));
+
+    fetchingId += PARALLEL_SCRAPES;
   }
 
-  console.log(`Scrapped all comics up to and including ${fetchingId - 3}.`);
+  const newLatestComic = await Comic.findOne({order: [['id', 'DESC']]});
+
+  if (newLatestComic) {
+    console.log(`Scrapped all comics up to and including ${newLatestComic.id}.`);
+  }
 };
