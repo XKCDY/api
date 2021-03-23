2535817867a8faeafe3d2da6eba5610a13da281f
Max Isom <hi@maxisom.me>
2021-03-22 20:42:37 -0400

    Add checks and Procfile

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,20 @@ import {ConfigModule} from '@nestjs/config';
 import {BullModule} from '@codetheweb/nestjs-bull';
 import {ServeStaticModule} from '@nestjs/serve-static';
 import {join} from 'path';
+import Redis from 'ioredis';
 import {ComicModule} from './comic/comic.module';
 import {DeviceTokenModule} from './device-token/device-token.module';
+
+const redisConnection = process.env.REDIS_URL ? new Redis(process.env.REDIS_URL) : {
+	port: Number.parseInt(process.env.REDIS_PORT!, 10),
+	host: process.env.REDIS_HOST
+};
 
 @Module({
 	imports: [
 		ConfigModule.forRoot(),
 		BullModule.forRoot({
-			connection: {
-				port: Number.parseInt(process.env.REDIS_PORT!, 10),
-				host: process.env.REDIS_HOST
-			}
+			connection: redisConnection
 		}),
 		ServeStaticModule.forRoot({
 			rootPath: join(__dirname, '..', 'public'),
