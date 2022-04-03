bf65483966dc49bdbda979adc6115b6b2b523c02
Max Isom <hi@maxisom.me>
2022-04-03 15:48:39 -0400

    Get key from env

--- src/lib/apn-provider-factory.ts
+++ src/lib/apn-provider-factory.ts
@@ -3,7 +3,7 @@ import apn from 'apn';
 const apnProviderFactory = () => {
 	return new apn.Provider({
 		token: {
-			key: process.env.KEY_PATH!,
+			key: process.env.KEY_PATH ?? Buffer.from(process.env.ENCODED_KEY ?? '', 'base64'),
 			keyId: process.env.KEY_ID!,
 			teamId: process.env.KEY_TEAM_ID!
 		}
