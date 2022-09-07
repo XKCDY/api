de60dd8c259aa2437449aed6711a606c0ab3e832
Max Isom <hi@maxisom.me>
2022-09-06 22:25:08 -0500

    Migrate main scrape job

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,14 @@ import Redis from 'ioredis';
 import {ComicModule} from './comic/comic.module';
 import {DeviceTokenModule} from './device-token/device-token.module';
 
-const redisConnection = process.env.REDIS_URL ? new Redis(process.env.REDIS_URL) : {
-	port: Number.parseInt(process.env.REDIS_PORT!, 10),
-	host: process.env.REDIS_HOST,
-};
-
 @Module({
 	imports: [
 		ConfigModule.forRoot(),
 		BullModule.forRoot({
-			connection: redisConnection,
+			connection: process.env.REDIS_URL ? new Redis(process.env.REDIS_URL) : {
+				port: Number.parseInt(process.env.REDIS_PORT!, 10),
+				host: process.env.REDIS_HOST,
+			},
 		}),
 		ServeStaticModule.forRoot({
 			rootPath: join(__dirname, '..', 'public'),
--- src/comic/processors/scrape-comics.ts
+++ src/comic/processors/scrape-comics.ts
@@ -4,8 +4,9 @@ import type {ProbeResult} from 'probe-image-size';
 import probe from 'probe-image-size';
 import {Logger} from '@nestjs/common';
 import * as exifr from 'exifr';
-import type {ComicImg, Prisma} from '@prisma/client';
-import {PrismaClient} from '@prisma/client';
+import {db} from 'src/lib/db';
+import type {DB} from 'kysely-codegen';
+import type {InsertObject} from 'kysely';
 
 const PARALLEL_SCRAPES = 5;
 
@@ -28,12 +29,115 @@ enum ImgSize {
 	x2 = 'x2'
 }
 
-const prisma = new PrismaClient();
-
 const getX2Url = (fromUrl: string): string => {
 	const imageUrlSplit = fromUrl.split('.');
 
 	return `${imageUrlSplit.splice(0, imageUrlSplit.length - 1).join('.')}_2x.${imageUrlSplit[imageUrlSplit.length - 1]}`;
+};
+
+const scrapeId = async (id: number) => {
+	const comic: XKCDResponse = await got(`https://xkcd.com/${id}/info.0.json`).json();
+
+	// Get images
+	const promises: Array<Promise<[PromiseSettledResult<ProbeResult>, PromiseSettledResult<ProbeResult>] | number>> = [
+		Promise.allSettled([
+			probe(comic.img),
+			probe(getX2Url(comic.img))
+		])
+	];
+
+	if (comic.img.includes('jpeg') || comic.img.includes('jpg')) {
+		promises.push((async () => {
+			const imgData = await got(comic.img).buffer();
+			return (await exifr.orientation(imgData)) ?? 1;
+		})());
+	}
+
+	const resolved = await Promise.all(promises);
+	const imgResults = resolved[0] as Array<PromiseSettledResult<ProbeResult>>;
+	const orientation = resolved[1] as number;
+
+	const swapWidthAndHeight = orientation !== undefined && [5, 6, 7, 8].includes(orientation);
+
+	const imgs: Array<InsertObject<DB, 'comic_imgs'>> = [];
+
+	for (const [i, result] of imgResults.entries()) {
+		if (result.status === 'fulfilled') {
+			const height = swapWidthAndHeight ? result.value.width : result.value.height;
+			const width = swapWidthAndHeight ? result.value.height : result.value.width;
+
+			imgs.push({
+				height,
+				width,
+				ratio: width / height,
+				sourceUrl: result.value.url,
+				size: i === 0 ? ImgSize.x1 : ImgSize.x2
+			} as InsertObject<DB, 'comic_imgs'>);
+		} else if (result.reason.statusCode !== 404) {
+			throw new Error(`Non-404 HTTP code: ${result.reason.statusCode as string}`);
+		}
+	}
+
+	await db.transaction().execute(async trx => {
+		const currentComic = await trx
+			.selectFrom('comics')
+			.where('id', '=', comic.num)
+			.selectAll()
+			.executeTakeFirst();
+
+		const data: InsertObject<DB, 'comics'> = {
+			id: comic.num,
+			publishedAt: new Date(`${comic.month}-${comic.day}-${comic.year}`),
+			news: comic.news,
+			safeTitle: comic.safe_title,
+			title: comic.title,
+			transcript: comic.transcript,
+			alt: comic.alt,
+			sourceUrl: `https://xkcd.com/${comic.num}`,
+			explainUrl: `https://www.explainxkcd.com/wiki/index.php/${comic.num}`
+		};
+
+		const updatePromises: Array<Promise<any>> = imgs.map(async img => {
+			const existingImage = await trx
+				.selectFrom('comic_imgs')
+				.where('comic_id', '=', data.id)
+				.where('size', '=', img.size)
+				.select('id')
+				.executeTakeFirst();
+
+			if (existingImage) {
+				await trx
+					.updateTable('comic_imgs')
+					.where('id', '=', existingImage.id)
+					.set(img as any)
+					.execute();
+			} else {
+				await trx
+					.insertInto('comic_imgs')
+					.values({
+						...img,
+						comic_id: data.id
+					})
+					.execute();
+			}
+		});
+		if (currentComic) {
+			updatePromises.push(trx
+				.updateTable('comics')
+				.where('id', '=', data.id)
+				.set(data)
+				.execute(),
+			);
+		} else {
+			updatePromises.push(trx
+				.insertInto('comics')
+				.values(data)
+				.execute(),
+			);
+		}
+
+		await Promise.all(updatePromises);
+	});
 };
 
 const logger = new Logger('Job: scrape comics');
@@ -42,7 +146,12 @@ const processJob = async (_: Job) => {
 	logger.log('Started scraping...');
 
 	// This could be significantly speed up on the first run by using binary search. Not really worth it at this point.
-	const latestComic = await prisma.comic.findFirst({orderBy: {id: 'desc'}});
+	const latestComic = await db
+		.selectFrom('comics')
+		.orderBy('id', 'desc')
+		.selectAll()
+		.executeTakeFirst();
+
 	let fetchingId = 1;
 
 	if (latestComic) {
@@ -77,101 +186,7 @@ const processJob = async (_: Job) => {
 		// eslint-disable-next-line no-await-in-loop
 		await Promise.all(idsToScrape.map(async id => {
 			try {
-				const comic: XKCDResponse = await got(`https://xkcd.com/${id}/info.0.json`).json();
-
-				// Get images
-				const promises: Array<Promise<[PromiseSettledResult<ProbeResult>, PromiseSettledResult<ProbeResult>] | number>> = [
-					Promise.allSettled([
-						probe(comic.img),
-						probe(getX2Url(comic.img))
-					])
-				];
-
-				if (comic.img.includes('jpeg') || comic.img.includes('jpg')) {
-					promises.push((async () => {
-						const imgData = await got(comic.img).buffer();
-						return (await exifr.orientation(imgData)) ?? 1;
-					})());
-				}
-
-				const resolved = await Promise.all(promises);
-				const imgResults = resolved[0] as Array<PromiseSettledResult<ProbeResult>>;
-				const orientation = resolved[1] as number;
-
-				const swapWidthAndHeight = orientation !== undefined && [5, 6, 7, 8].includes(orientation);
-
-				const imgs: ComicImg[] = [];
-
-				for (const [i, result] of imgResults.entries()) {
-					if (result.status === 'fulfilled') {
-						const height = swapWidthAndHeight ? result.value.width : result.value.height;
-						const width = swapWidthAndHeight ? result.value.height : result.value.width;
-
-						imgs.push({
-							height,
-							width,
-							ratio: width / height,
-							sourceUrl: result.value.url,
-							size: i === 0 ? ImgSize.x1 : ImgSize.x2
-						} as ComicImg);
-					} else if (result.reason.statusCode !== 404) {
-						throw new Error(`Non-404 HTTP code: ${result.reason.statusCode as string}`);
-					}
-				}
-
-				const currentComic = await prisma.comic.findUnique({
-					where: {id: comic.num},
-					select: {imgs: true}
-				});
-
-				const data: Prisma.ComicCreateInput = {
-					id: comic.num,
-					publishedAt: new Date(`${comic.month}-${comic.day}-${comic.year}`),
-					news: comic.news,
-					safeTitle: comic.safe_title,
-					title: comic.title,
-					transcript: comic.transcript,
-					alt: comic.alt,
-					sourceUrl: `https://xkcd.com/${comic.num}`,
-					explainUrl: `https://www.explainxkcd.com/wiki/index.php/${comic.num}`
-				};
-
-				if (currentComic) {
-					await Promise.all<any>([
-						prisma.comic.update({
-							where: {id: data.id},
-							data
-						}),
-						...imgs.map(async img => {
-							const existingImage = await prisma.comicImg.findFirst({
-								where: {
-									comic_id: data.id,
-									size: img.size
-								}
-							});
-
-							if (existingImage) {
-								await prisma.comicImg.update({
-									where: {id: existingImage.id},
-									data: img
-								});
-							} else {
-								await prisma.comicImg.create({
-									data: {
-										...img,
-										comic_id: data.id
-									}
-								});
-							}
-						})
-					]);
-				} else {
-					data.imgs = {
-						create: imgs
-					};
-
-					await prisma.comic.create({data});
-				}
+				await scrapeId(id);
 			} catch (error: unknown) {
 				if (error instanceof HTTPError) {
 					if (error.response.statusCode === 404) {
@@ -186,13 +201,9 @@ const processJob = async (_: Job) => {
 		fetchingId += PARALLEL_SCRAPES;
 	}
 
-	const newLatestComic = await prisma.comic.findFirst({orderBy: {id: 'desc'}});
+	const {id: scrapedUpToId} = await db.selectFrom('comics').orderBy('id', 'desc').select('id').executeTakeFirstOrThrow();
 
-	if (newLatestComic) {
-		logger.log(`Scrapped all comics up to and including ${newLatestComic.id}.`);
-	}
-
-	await prisma.$disconnect();
+	logger.log(`Scrapped all comics up to and including ${scrapedUpToId}.`);
 };
 
 export default processJob;
